6a7fc697a0b0826c6a5770a8a1c35ad8f401d0a2
nwlilley <nathaniel.lilley@gmail.com>
2020-02-13 13:21:40 -0500

    defined edges of gamefield for player

--- script.js
+++ script.js
@@ -16,34 +16,53 @@ class Game {
   update () {
     console.log('update')
     this.player.update()
+    // this.enemy.update()
   }
 
   draw (screen, gameSize) {
-    // screen.fillRect(20, 50, 50, 50)
     screen.clearRect(0, 0, gameSize.x, gameSize.y)
     drawRect(screen, this.player)
+    // drawRect(screen, this.enemy)
   }
+}
+
+class Enemy {
+  constructor (game, gameSize) {
+    this.game = game
+    this.center = center
+    this.size = { x: 15, y: 15 }
+    this.patrolX = 0
+    this.speedX = 0.3
+  }
+
+  update () {
+    if (this.patrolX < 0 || this.patrolX > 30) {
+      this.speedX = -this.speedX
+    }
+  }
+  // if (Math.random() > 0.995 && !this.game.)
 }
 
 class Player {
   constructor (game, gameSize) {
     this.game = game
-    this.size = { x: 20, y: 20 }
-    this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.y * 2 }
+    this.size = { x: 400, y: 400 }
+    this.center = { x: gameSize.x / 2, y: gameSize.y / 2 }
+    this.gameSize = gameSize
     this.keyboarder = Keyboarder
   }
 
   update () {
-    if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+    if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT) && (this.center.x > (0 + this.size.x / 2))) {
       this.center.x -= 2
       console.log('left')
-    } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+    } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT) && (this.center.x < this.gameSize.x - this.size.x / 2)) {
       this.center.x += 2
       console.log('right')
-    } else if (this.keyboarder.isDown(this.keyboarder.KEYS.UP)) {
+    } else if (this.keyboarder.isDown(this.keyboarder.KEYS.UP) && (this.center.y > (0 + this.size.x / 2))) {
       this.center.y -= 2
       console.log('up')
-    } else if (this.keyboarder.isDown(this.keyboarder.KEYS.DOWN)) {
+    } else if (this.keyboarder.isDown(this.keyboarder.KEYS.DOWN) && (this.center.y < (this.gameSize.y - this.size.y / 2))) {
       this.center.y += 2
       console.log('down')
     }
@@ -55,31 +74,13 @@ class Player {
 function drawRect (screen, body) {
   screen.fillRect(body.center.x - body.size.x / 2, body.center.y - body.size.y / 2,
     body.size.x, body.size.y)
-  }
-// class Keyboarder {
-//   constructor () {
-//     // Records up/down state of each key that has ever been pressed.
-//     const keyState = {}
+}
 
-//     // When key goes down, record that it is down.
-//     window.addEventListener('keydown', function (e) {
-//       keyState[e.keyCode] = true
-//     })
-
-//     // When key goes up, record that it is up.
-//     window.addEventListener('keyup', function (e) {
-//       keyState[e.keyCode] = false
-//     })
-
-//     // Returns true if passed key is currently down.  `keyCode` is a
-//     // unique number that represents a particular key on the keyboard.
-//     this.isDown = function (keyCode) {
-//       return keyState[keyCode] === true
-//     }
-
-//     // Handy constants that give keyCodes human-readable names.
-//     this.KEYS = { LEFT: 37, RIGHT: 39, UP: 38, DOWN: 40, S: 83 }
-//   }
+// function drawCircle (screen, body) {
+//   screen.arc(body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.radius, 0, Math.PI * 2, true)
 // }
 
+// function create 
+
 const game = new Game()
+
