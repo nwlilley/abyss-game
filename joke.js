120f3634e7a93fb0a3a8c28c4748c3b9de7eee28
nwlilley <nathaniel.lilley@gmail.com>
2020-02-16 16:47:49 -0500

    switch to circles

--- joke.js
+++ joke.js
@@ -23,6 +23,7 @@ class Game {
   update () {
     this.player.update()
     this.enemy.update()
+    distance(this.player, this.enemy)
     if (colliding(this.player, this.enemy)) {
       console.log('colliding')
       addMessage()
@@ -59,8 +60,8 @@ class Enemy {
   constructor (game, gameSize) {
     this.game = game
     this.size = { x: 50, y: 50 }
-    this.radius = this.size.x / 2
-    this.center = { x: this.size.x, y: this.size.y }
+    this.radius = 25
+    this.center = { x: 50, y: 50}
     this.gameSize = gameSize
     this.moveX = 0
     this.velocity = 1.5
@@ -73,12 +74,12 @@ class Enemy {
     // this.center.x += Math.random() * this.velocity * posOrNeg()
     // this.center.y += Math.random() * this.velocity * posOrNeg()
     // this.center.x += this.velocity
-    if (this.center.x < 0 || this.center.x > this.gameSize.x) {
-      this.center.x *= -1
-    }
-    if (this.center.y < 0 || this.center.y > this.gameSize.y) {
-      this.center.y *= -1
-    }
+    // if (this.center.x < 0 || this.center.x > this.gameSize.x) {
+    //   this.center.x *= -1
+    // }
+    // if (this.center.y < 0 || this.center.y > this.gameSize.y) {
+    //   this.center.y *= -1
+    // }
 
 
 
@@ -107,40 +108,56 @@ class Player {
   constructor (game, gameSize) {
     this.game = game
     this.size = { x: 40, y: 40 }
-    this.radius = this.size.x / 2
-    this.center = { x: gameSize.x - this.size.x, y: gameSize.y - this.size.y }
+    this.radius = 20
+    this.center = { x: gameSize.x, y: gameSize.y }
     this.gameSize = gameSize
     this.keyboarder = Keyboarder
   }
 
   update () {
-    if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT) && (this.center.x > (0 + this.size.x / 2))) {
-      this.center.x -= 3
-      console.log('left')
-    } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT) && (this.center.x < this.gameSize.x - this.size.x / 2)) {
-      this.center.x += 3
-      console.log('right')
+    if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT) && (this.center.x > (0 + this.radius * 2))) {
+      this.center.x -= 2
+      // console.log('left')
+      // console.log (this.center)
+    } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT) && (this.center.x < this.gameSize.x - this.radius)) {
+      this.center.x += 2
+      // console.log('right')
     } else if (this.keyboarder.isDown(this.keyboarder.KEYS.UP) && (this.center.y > (0 + this.size.x / 2))) {
-      this.center.y -= 3
-      console.log('up')
+      this.center.y -= 2
+      // console.log('up')
     } else if (this.keyboarder.isDown(this.keyboarder.KEYS.DOWN) && (this.center.y < (this.gameSize.y - this.size.y / 2))) {
-      this.center.y += 3
-      console.log('down')
+      this.center.y += 2
+      // console.log('down')
     }
   }
 }
 
 // XXXXXXXXXXXXXXXXX
 
-function colliding (b1, b2) {
-  return !(
-    b1 === b2 ||
-      b1.center.x + b1.size.x / 2 < b2.center.x - b2.radius ||
-      b1.center.y + b1.size.y / 2 < b2.center.y - b2.radius ||
-      b1.center.x - b1.size.x / 2 > b2.center.x + b2.radius ||
-      b1.center.y - b1.size.y / 2 > b2.center.y + b2.radius
-  )
+function distance (player, enemy) {
+  let pos1 = Math.abs(player.center.x - enemy.center.x)
+  let pos2 = Math.abs(player.center.y - enemy.center.y)
+  let hypotenuse = Math.sqrt(pos1 ** 2 + pos2 ** 2)
+  console.log(hypotenuse)
+  return hypotenuse
 }
+
+function colliding (player, enemy) {
+  if (distance (player, enemy) <= player.radius + enemy.radius)
+    console.log('baby butt')// console.log(enemy.radius)
+  // console.log('distance x: ' + distX)
+  // console.log('distance y: ' + distY)
+  
+  // return !(
+  //   b1 === b2 ||
+  //     b1.center.x + b1.size.x / 2 < b2.center.x - b2.radius ||
+  //     b1.center.y + b1.size.y / 2 < b2.center.y - b2.radius ||
+  //     b1.center.x - b1.size.x / 2 > b2.center.x + b2.radius ||
+  //     b1.center.y - b1.size.y / 2 > b2.center.y + b2.radius
+  // )
+}
+
+
 
 function drawRect (screen, body) {
   screen.fillRect(body.center.x - body.size.x / 2, body.center.y - body.size.y / 2,
@@ -148,8 +165,10 @@ function drawRect (screen, body) {
 }
 
 function drawCircle (screen, body) {
+  screen.beginPath()
   screen.arc(body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.radius, 0, Math.PI * 2, true)
-  screen.fill()
+  screen.stroke()
+  // screen.fill()
 }
 
 function getRandInt (min, max) {
